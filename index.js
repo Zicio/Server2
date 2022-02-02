324ccdfa2ba03ccdea4adda854bfb7f2422a7841
Alexander Burachkov <zicio@yandex.ru>
2022-02-02 20:42:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -30,33 +30,6 @@ const messages = [
   { name: 'BBB', text: 'Hi' }
 ];
 
-// !На будушее
-// const format = date => {
-//   if (date < 10) {
-//     date = '0' + date;
-//   }
-//   return date;
-// };
-
-// const getDate = () => {
-//   const date = new Date();
-//   const month = format(date.getMonth() + 1);
-//   const day = format(date.getDate());
-//   let hour = format(date.getHours() + 4);
-//   if (hour === 24) {
-//     hour = 0;
-//   }
-//   const minute = format(date.getMinutes());
-//   const year = +date.getFullYear().toString().slice(2);
-//   return {
-//     month,
-//     day,
-//     hour,
-//     minute,
-//     year
-//   };
-// };
-
 router.get('/users/:name', async ctx => {
   const index = users.findIndex(({ name }) => name === ctx.params.name);
   if (index === -1) {
